d627ef849f90bb2aac0cdfb281b5f3559bb46645
daviddeladem <33095955+daviddeladem@users.noreply.github.com>
2022-07-15 08:16:23 +0000

    added csv import

--- app/Http/Controllers/API/ContactController.php
+++ app/Http/Controllers/API/ContactController.php
@@ -18,7 +18,7 @@ class ContactController extends BaseController
      */
     public function index()
     {
-        $contacts = Contact::all();
+        $contacts = Contact::latest()->paginate(20);
     
         return $this->sendResponse(ContactResource::collection($contacts), 'Contacts retrieved successfully.');
     }
@@ -101,5 +101,59 @@ class ContactController extends BaseController
         $contact->delete();
    
         return $this->sendResponse([], 'Contact deleted successfully.');
+    }
+
+    private $rows = [];
+    
+    public function import(Request $request)
+    {
+        $path = $request->file('file')->getRealPath();
+        $records = array_map('str_getcsv', file($path));
+
+        if (! count($records) > 0) {
+           return 'Error...';
+        }
+
+        // Get field names from header column
+        $fields = array_map('strtolower', $records[0]);
+
+        // Remove the header column
+        array_shift($records);
+
+        foreach ($records as $record) {
+            if (count($fields) != count($record)) {
+                return 'csv_upload_invalid_data';
+            }
+
+            // Decode unwanted html entities
+            $record =  array_map("html_entity_decode", $record);
+
+            // Set the field name as key
+            $record = array_combine($fields, $record);
+
+            // Get the clean data
+            $this->rows[] = $this->clear_encoding_str($record);
+        }
+
+        foreach ($this->rows as $data) {
+            Conatct::create([
+               'name' => $data['name'],
+               'phone' => $data['phone'],
+            ]);
+        }
+
+        return $this->sendResponse(new ContactResource($contact), 'Contact uploaded successfully.');
+    }
+    
+    private function clear_encoding_str($value)
+    {
+        if (is_array($value)) {
+            $clean = [];
+            foreach ($value as $key => $val) {
+                $clean[$key] = mb_convert_encoding($val, 'UTF-8', 'UTF-8');
+            }
+            return $clean;
+        }
+        return mb_convert_encoding($value, 'UTF-8', 'UTF-8');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('contacts', ContactController::class);
+Route::post('/import',[ContactController::class, 'import'])->name('user.import');
